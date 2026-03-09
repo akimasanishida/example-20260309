c91da2f994577585b611a0bd40abe967c901f827
NARITA KANAKO <naritakn@mana01.ana.nao.ac.jp>
2026-03-09 10:58:35 +0900

    Modify main.py Add one phrase

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello World!")
-rint("This is a test.")  # bug: rint should be print
-pint("This is another test.")  # bug: pint should be print
-
+print("This is a test.")  # bug: rint should be print
+print("This is another test.")  # bug: pint should be print
+print("My name is Kanako.")
